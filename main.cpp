96d4bd18288f19c27ec5f6ec07d12068a2a484ce
nullptroot <1611576808@qq.com>
2023-10-31 16:38:24 +0800

    把webserver、config类也抄完了，剩下的就是改进项目了

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "config.h"
+#include <iostream>
+using std::cout;
+
+int main(int argc,char *argv[])
+{
+    string user = "root";
+    string passwd = "Mrchen613";
+    string databasename = "webServer";
+
+    Config config;
+    config.parse_arg(argc,argv);
+    
+    WebServer server;
+    
+    server.init(config.PORT,user,passwd,databasename,config.LOGWrite,
+                    config.OPT_LINGER,config.TRIGMode,config.sql_num,
+                    config.thread_num,config.close_log,config.actor_model);
+    
+    server.log_write();
+    
+
+    server.sql_pool();
+    
+
+    server.thread_pool();
+    
+
+    server.trig_mode();
+    
+
+    server.eventListen();
+    
+
+    server.eventLoop();
+    
+
+    return 0;
+}
---
+++ webserver.h
@@ -0,0 +1,78 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+
+
+#include "./threadpool/threadpool_11.h"
+#include "./http/http_conn.h"
+const int MAX_FD = 65535;
+const int MAX_EVENT_NUMBER = 10000;
+const int TIMESLOT = 5;
+
+class WebServer
+{
+    public:
+        int m_port;
+        char *m_root;
+        int m_log_write;
+        int m_close_log;
+        int m_actormodel;
+
+        int m_pipefd[2];
+        int m_epollfd;
+        http_conn *users;
+
+        connection_pool *m_connPool;
+        std::string m_user;
+        std::string m_passWord;
+        std::string m_databaseName;
+        int m_sql_num;
+
+        threadpool<http_conn> *m_pool;
+
+        int m_thread_num;
+
+        epoll_event events[MAX_EVENT_NUMBER];
+
+        int m_listenfd;
+        int m_OPT_LINGER;
+
+        int m_TRIGMode;
+        int m_LISTENTigmode;
+        int m_CONNTrigmode;
+
+        client_data *users_timer;
+        Utils utils;
+    public:
+        WebServer();
+        ~WebServer();
+
+        void init(int port,std::string user,std::string passWord,std::string databaseName,
+                    int log_write,int opt_linger,int trigmode,int sql_num,
+                    int thread_num,int close_log,int actor_model);
+        void thread_pool();
+        void sql_pool();
+        void log_write();
+        void trig_mode();
+        void eventListen();
+        void eventLoop();
+        void timer(int connfd,struct sockaddr_in client_address);
+        void adjust_timer(util_timer *timer);
+        void deal_timer(util_timer *timer,int sockfd);
+        bool dealclientdata();
+        bool dealwithsignal(bool &timeout,bool &stop_server);
+        void dealwithread(int sockfd);
+        void dealwithwrite(int sockfd);
+};
+
+#endif
